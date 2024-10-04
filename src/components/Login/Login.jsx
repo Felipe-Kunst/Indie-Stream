067a00bd6d959f8ce3f17ddf57e210f3d07f8fbd
Luis Kaio <ytkkaio16@gmail.com>
2024-10-04 08:21:19 -0300

    linkando back login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,38 +1,54 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useCookies } from 'react-cookie';
-import styles from './Login.module.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import styles from "./Login.module.css";
 import Logo from "../../assets/Logo.png";
 
 function Login() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [error, setError] = useState('');
-  const [cookies, setCookie] = useCookies(['userId']);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [cookies, setCookie] = useCookies(["userId"]);
   const navigate = useNavigate();
 
   const handleLogin = async (event) => {
     event.preventDefault();
+    // console.log("Tentativa de login", email, password);
+
+    if (!email || !password) {
+      setError("Preencha todos os campos.");
+      return;
+    }
 
     try {
-      const response = await fetch(`http://localhost:3002/usuarios?email=${email}`);
-      const data = await response.json();
+      console.log("Iniciando fetch...");
 
-      if (data.length > 0) {
-        const loggedInUser = data[0];
+      const response = await fetch("http://localhost:8080/user/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: email,
+          senha: password,
+        }),
+      });
 
-        if (loggedInUser.senha === password) {
-          setError('');
-          setCookie('userId', loggedInUser.id, { path: '/', maxAge: 86400 }); 
-          navigate('/HomelogadoPage');
-        } else {
-          setError('Senha incorreta.');
-        }
+      console.log("Resposta recebida:", response);
+
+      if (response.ok) {
+        const data = await response.json();
+        setError("");
+        setCookie("userId", data.id, { path: "/", maxAge: 86400 });
+        navigate("/HomelogadoPage");
+      } else if (response.status === 401) {
+        setError("Credenciais inválidas.");
       } else {
-        setError('Email não encontrado.');
+        setError("Erro ao tentar realizar o login.");
       }
     } catch (error) {
-      setError('Erro ao tentar realizar o login.');
+      console.error("Erro durante o fetch:", error);
+      setError("Erro ao tentar realizar o login.");
     }
   };
 
@@ -44,33 +60,46 @@ function Login() {
 
         <div className={styles.grupoInput}>
           <label htmlFor="email">Email</label>
-          <input 
-            type="email" 
-            id="email" 
-            placeholder="Insira seu email" 
-            value={email} 
-            onChange={(e) => setEmail(e.target.value)} 
-            className={error ? `${styles.input} ${styles.inputErro}` : styles.input}
+          <input
+            type="email"
+            id="email"
+            placeholder="Insira seu email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className={
+              error ? `${styles.input} ${styles.inputErro}` : styles.input
+            }
           />
         </div>
 
         <div className={styles.grupoInput}>
           <label htmlFor="password">Senha</label>
-          <input 
-            type="password" 
-            id="password" 
-            placeholder="Insira sua senha" 
-            value={password} 
-            onChange={(e) => setPassword(e.target.value)} 
-            className={error ? `${styles.input} ${styles.inputErro}` : styles.input}
+          <input
+            type="password"
+            id="password"
+            placeholder="Insira sua senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className={
+              error ? `${styles.input} ${styles.inputErro}` : styles.input
+            }
           />
         </div>
 
-        <a href="/EsqueceuSenha" className={styles.linkEsqueciSenha}>Esqueceu sua senha?</a>
-        
-        <button type="submit" className={styles.botao}>Login</button>
+        <a href="/EsqueceuSenha" className={styles.linkEsqueciSenha}>
+          Esqueceu sua senha?
+        </a>
 
-        <p className={styles.text}>Não possui conta? <a href="/PaginadeCadastro" className={styles.link}>Cadastre-se</a></p>
+        <button type="submit" className={styles.botao}>
+          Login
+        </button>
+
+        <p className={styles.text}>
+          Não possui conta?{" "}
+          <a href="/PaginadeCadastro" className={styles.link}>
+            Cadastre-se
+          </a>
+        </p>
       </form>
     </div>
   );
