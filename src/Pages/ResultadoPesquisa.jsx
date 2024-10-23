d22f86117d5307f8674e991056cb0a015add2c76
Luis Kaio <ytkkaio16@gmail.com>
2024-10-23 10:29:42 -0300

    ajustes css e adição header e footer pagina de resultados

--- src/Pages/ResultadoPesquisa.jsx
+++ src/Pages/ResultadoPesquisa.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 
-import ResultadosPesquisa from './../components/ResultadoPesquisa/ResultadoPesquisa';
+import ResultadosPesquisa from "./../components/ResultadoPesquisa/ResultadoPesquisa";
+import Flutter from "../components/Flutter/Flutter";
+import HeaderLogado from "../components/HeaderLogado/HeaderLogado";
 
 export default function Main() {
   return (
-    <div>
-      <ResultadosPesquisa/>
+    <div style={{ paddingTop: "95px" }}>
+      {/* Espaço para o header fixo */}
+      <HeaderLogado />
+      <ResultadosPesquisa />
+      <Flutter />
     </div>
   );
 }
-
-
